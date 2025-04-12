67b174730cbdb14c839249e5eb8551d0dcc2c436
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-12 11:52:47 -0300

    separate component with table title

---
+++ src/components/Table/TableTitle/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export const TableTitle = () => {
+    return (
+        <tr>
+            <th className='titleTh'>Nomes</th>
+            <th className='titleTh'>Ações</th>
+        </tr>
+    )
+}
