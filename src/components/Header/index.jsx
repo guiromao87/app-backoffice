2433528ab1d33f07b7789232af42feeed9426e87
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-14 14:19:24 -0300

    component with side menu and header navigation removed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,21 @@
-import { Link } from 'react-router-dom';
-import './index.css'
+import './index.css';
 
 const Header = () => {
 
     const name = sessionStorage.getItem('name');
 
-    return ( 
+    return (
         <header>
             <nav className="nav__header">
-            <div className='user__logged'>
-                    <b>Usuário:</b> {name}
-                </div> 
-                <div className='menu'>
-                    <ul>
-                        <li><Link to="/profissionais">Profissionais</Link></li>
-                        <li><Link to="/abordagens">Abordagens</Link></li>
-                        <li><Link to="/especialidades">Especialidades</Link></li>
-                    </ul>
-                </div>  
                 <div className='user__logged'>
-                    <Link to="/sair">Logout</Link>
-                </div> 
+                    <b>Backoffice - Cloud4Care</b>
+                </div>
+                <div className='menu'>
+
+                </div>
+                <div className='user__logged'>
+                    <p>{name}</p>
+                </div>
             </nav>
         </header>
     )
