4c4f81090eea526c495e755d35dd5a23aa5b0d21
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-14 14:17:23 -0300

    structure with side menu for all pages

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,8 +1,19 @@
 import { Navigate } from "react-router-dom";
 import Header from "../components/Header/index";
+import { AsideMenu } from "../components/AsideMenu";
 
 export const PrivateRoute = ({ Component }) => {
     const isAuthenticated = sessionStorage.getItem('token');
 
-    return isAuthenticated ? <> <Header /> <Component /> </> : <Navigate to="/" />;
+    return isAuthenticated ? (
+        <>
+            <Header />
+            <div className="app-layout">
+                <AsideMenu />
+                <main className="page-content">
+                    <Component />
+                </main>
+            </div>
+        </>
+    ) : <Navigate to="/" />;
 }
