a9ea2dedc4148622af7c94d18a8f8e1ef05428bb
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-15 10:12:26 -0300

    emptying the home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,8 @@
 const Home = () => {
     
-    const name = sessionStorage.getItem('name');
-    const email = sessionStorage.getItem('email');
-    
     return (
         <div>
-            <h2>Home</h2>
-                <div>
-                    Nome: {name}
-                </div>
-                <div>
-                    Email: {email}
-                </div>
+
         </div>
     )
 }
