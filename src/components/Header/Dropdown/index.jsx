098cf911d4b84f118c0bc246a442e9582c6713a0
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-29 09:00:16 -0300

    added when clicking outside the dropdown menu, close it

--- src/components/Header/Dropdown/index.jsx
+++ src/components/Header/Dropdown/index.jsx
@@ -2,8 +2,9 @@ import { FiLogOut } from "react-icons/fi";
 import { RxAvatar } from "react-icons/rx";
 import { NavLink, useNavigate } from 'react-router-dom';
 import './index.css';
+import { forwardRef } from "react";
 
-export const Dropdown = () => {
+export const Dropdown = forwardRef((props, ref) => {
     const navigate = useNavigate();
     const handleLogout = async () => {
         // await api.post({ endpoint: '/logout?refreshToken=' }); //TODO: preciso arrumar o refresh token para enviar ele
@@ -12,7 +13,7 @@ export const Dropdown = () => {
     }
 
     return (
-        <div className='dropdown-header'>
+        <div {...props} ref={ref} className='dropdown-header'>
             <NavLink to="/home" className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}>
                 <RxAvatar className='avatar-dropdown' />
                 Perfil
@@ -23,4 +24,4 @@ export const Dropdown = () => {
             </div>
         </div>
     )
-}
+})
