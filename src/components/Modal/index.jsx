a18afcc71605ca0239daa3863ddc7ae5bc7b4b39
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-06 11:57:18 -0300

    modified keys in modal and table

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,55 +1,67 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import './index.css';
-import { Toggle } from "./Toggle";
+import { Toggle } from './Toggle';
 
-export const Modal = ({ placeholder, title, defaultData, onSubmit, onClose, error, showInput = true, showToggle = false }) => {
-    const [input, setInput] = useState("");
-    const [status, setStatus] = useState(true);
-    const [isAdmin, setIsAdmin] = useState(false);
+export const Modal = ({
+  placeholder,
+  title,
+  defaultData,
+  onSubmit,
+  onClose,
+  error,
+  showInput = true,
+  showToggle = false,
+}) => {
+  const [input, setInput] = useState('');
+  const [ativo, setAtivo] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        onSubmit({ name: input, status: status, admin: isAdmin });
+    onSubmit({ nome: input, ativo: ativo, admin: isAdmin });
+  };
+
+  useEffect(() => {
+    if (defaultData) {
+      setInput(defaultData.nome);
+      setAtivo(defaultData.ativo);
+      setIsAdmin(defaultData.admin);
+    } else {
+      setAtivo(true);
+      setIsAdmin(false);
     }
+  }, [defaultData]);
 
-    useEffect(() => {
-        if (defaultData) {
-            setInput(defaultData.name);
-            setStatus(defaultData.status);
-            setIsAdmin(defaultData.amin);
-        } else {
-            setStatus(true);
-            setIsAdmin(false);
-        }
-    }, [defaultData]);
+  return (
+    <form onSubmit={handleSubmit} id="modal">
+      <h3>{title}</h3>
 
-    return (
-        <form onSubmit={handleSubmit} id="modal">
-            <h3>{title}</h3>
+      {showInput && (
+        <input
+          required
+          value={input}
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      )}
 
-            {showInput && (
-                <input
-                    required
-                    value={input}
-                    type="text"
-                    placeholder={placeholder}
-                    onChange={(e) => setInput(e.target.value)} />
-            )}
+      {(defaultData || showToggle) && (
+        <>
+          <Toggle text="Ativar:" setToggle={setAtivo} toggle={ativo} />
+          <Toggle text="Admin:" setToggle={setIsAdmin} toggle={isAdmin} />
+        </>
+      )}
 
-            {(defaultData || showToggle) && (
-                <>
-                    <Toggle text='Ativar:' setToggle={setStatus} toggle={status} />
-                    <Toggle text='Admin:' setToggle={setIsAdmin} toggle={isAdmin} />
-                </>
-            )}
+      {error && <p className="error-message">{error}</p>}
 
-            {error && <p className="error-message">{error}</p>}
-
-            <div className="button-group">
-                <button type="submit">{defaultData ? "Salvar" : "Adicionar"}</button>
-                <button type="button" onClick={onClose}>Cancelar</button>
-            </div>
-        </form >
-    )
-}
+      <div className="button-group">
+        <button type="submit">{defaultData ? 'Salvar' : 'Adicionar'}</button>
+        <button type="button" onClick={onClose}>
+          Cancelar
+        </button>
+      </div>
+    </form>
+  );
+};
--- src/components/Table/TableData/index.jsx
+++ src/components/Table/TableData/index.jsx
@@ -1,32 +1,30 @@
 import './index.css';
 
 export const TableData = ({ data, columns, actions = [] }) => {
-    const formatValue = (col) => {
-        if (col === "status") {
-            return data[col] ? "Ativo" : "Inativo";
-        }
-        else if (col === "admin") {
-            return data[col] ? "Sim" : "Não";
-        }
-        else if (col === "createdAt") {
-            return new Date(data[col]).toLocaleDateString('pt-BR');
-        }
-        return data[col]
+  const formatValue = (col) => {
+    if (col === 'ativo') {
+      return data[col] ? 'Ativo' : 'Inativo';
+    } else if (col === 'admin') {
+      return data[col] ? 'Sim' : 'Não';
+    } else if (col === 'createdAt') {
+      return new Date(data[col]).toLocaleDateString('pt-BR');
     }
+    return data[col];
+  };
 
-    return (
-        <tr>
-            {columns.map((col, i) => (
-                <td key={i} className="td-content">
-                    {formatValue(col)}
-                </td>
-            ))}
+  return (
+    <tr>
+      {columns.map((col, i) => (
+        <td key={i} className="td-content">
+          {formatValue(col)}
+        </td>
+      ))}
 
-            <td>
-                {actions.map(({ icon: Icon, onClick }, i) => (
-                    <Icon key={i} className="iconTd" onClick={onClick} />
-                ))}
-            </td>
-        </tr>
-    )
-}
+      <td>
+        {actions.map(({ icon: Icon, onClick }, i) => (
+          <Icon key={i} className="iconTd" onClick={onClick} />
+        ))}
+      </td>
+    </tr>
+  );
+};
