c7e37dfbcf19249e375a863fdaf986f40a904419
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-06 09:21:25 -0300

    modified backend url port

--- src/services/config.js
+++ src/services/config.js
@@ -1,43 +1,47 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:8080/api/v1"
+const BASE_URL = 'http://localhost:8081/api/v1';
 
 const api = axios.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+});
 
-api.interceptors.request.use((request) => {
+api.interceptors.request.use(
+  (request) => {
     const token = sessionStorage.getItem('token');
     if (token) {
-        request.headers['Authorization'] = `Bearer ${token}`;
+      request.headers['Authorization'] = `Bearer ${token}`;
     }
     return request;
-}, error => {
+  },
+  (error) => {
     return Promise.reject(error);
-})
+  }
+);
 
-api.interceptors.response.use((response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-        const refreshToken = sessionStorage.getItem('refresh');
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = sessionStorage.getItem('refresh');
 
-        if (error?.response?.status === 401 && !originalRequest?.__isRetryRequest) {
-            originalRequest.__isRetryRequest = true;
+    if (error?.response?.status === 401 && !originalRequest?.__isRetryRequest) {
+      originalRequest.__isRetryRequest = true;
 
-            const res = await axios.post(`${BASE_URL}/refresh-token?refreshToken=${refreshToken}`)
+      const res = await axios.post(`${BASE_URL}/refresh-token?refreshToken=${refreshToken}`);
 
-            const newToken = res.data.accessToken;
-            const newRefresh = res.data.refreshToken;
+      const newToken = res.data.accessToken;
+      const newRefresh = res.data.refreshToken;
 
-            sessionStorage.setItem("token", newToken);
-            sessionStorage.setItem("refresh", newRefresh);
-            
-            originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+      sessionStorage.setItem('token', newToken);
+      sessionStorage.setItem('refresh', newRefresh);
 
-            return api(originalRequest);
-        }
-        return Promise.reject(error);
+      originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+
+      return api(originalRequest);
     }
-)
+    return Promise.reject(error);
+  }
+);
 
 export default api;
