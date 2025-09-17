602d8ee179e670f4f9c89b48e941fe223d44b74a
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-17 13:33:28 -0300

    removed Profile item from Dropdown

--- src/components/Header/Dropdown/index.jsx
+++ src/components/Header/Dropdown/index.jsx
@@ -1,27 +1,20 @@
-import { FiLogOut } from "react-icons/fi";
-import { RxAvatar } from "react-icons/rx";
-import { NavLink, useNavigate } from 'react-router-dom';
-import './index.css';
-import api from "../../../services/config";
 import { forwardRef } from "react";
-
+import { FiLogOut } from "react-icons/fi";
+import { useNavigate } from 'react-router-dom';
+import './index.css';
 
 export const Dropdown = forwardRef((props, ref) => {
     const navigate = useNavigate();
     const handleLogout = async () => {
         const refresh = sessionStorage.getItem('refresh')
 
-        await api.post(`/logout?refreshToken=${refresh}`);
+        // await api.post(`/logout?refreshToken=${refresh}`);
         sessionStorage.clear();
         navigate('/');
     }
 
     return (
         <div {...props} ref={ref} className='dropdown-header'>
-            <NavLink to="/home" className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}>
-                <RxAvatar className='avatar-dropdown' />
-                Perfil
-            </NavLink>
             <div className="menu-link" onClick={handleLogout}>
                 <FiLogOut style={{ marginRight: "0.5rem" }} />
                 Logout
